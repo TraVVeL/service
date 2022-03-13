feea81fce760c6bd774079b7f52561e245287ace
TraVVeL <56757288+TraVVeL@users.noreply.github.com>
2022-03-13 22:56:05 +0700

    Update webservice.py

--- webservice.py
+++ webservice.py
@@ -1,30 +1,26 @@
 from flask import Flask, request
 
+
 app = Flask(__name__)
+
 
 @app.route('/')
 def index():
     return 'Start page'
 
 @app.route('/route', methods=['GET', 'POST'])
-def login():
+def route():
+    rules = ['No primary parameter', 'No secondary parameter']
     name = request.args.get('name')
     message = request.args.get('message')
 
-    if name != None and message != None:
-        return f"Hello {name}! {message}!"
+    return f"{rules[0] if name == None else 'Hello ' + name + '!'} {rules[1] if message == None else message + '!'}"
 
-    elif name == None and message != None:
-        return f"{message}"
 
-    elif name != None and message == None:
-        return f"Hello {name}!"
-
-    else:
-        return "There is no parameters"
+@app.errorhandler(404)
+def page_not_found(error):
+    return 'Bad request'
 
 
 if __name__ == '__main__':
     app.run(debug=True)
-
-
